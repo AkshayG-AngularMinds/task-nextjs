13a35d006ab11b53b1ab3ee14875b38153758d50
AMGAkshay <akshay.gawade@angularminds.in>
2024-05-13 14:27:25 +0530

    change route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Header from "./Header";
+import { useEffect } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Header />
+      {/* <Header /> */}
     </main>
   );
 }
